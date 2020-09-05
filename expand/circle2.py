621532cfb549eafe55cc858ee1544d96d12ea6cd
jacksparrowFF2 <s.ph@outlook.com>
2020-09-05 09:28:40 +0800

    update

--- expand/circle2.py
+++ expand/circle2.py
@@ -16,7 +16,53 @@ import numpy as np
 import xlwings as xw
 import argparse
 # hear is the code 
-# 创建 Excel
+""" # 函数构建
+# 切片次数
+t = 10
+n = 90
+# 生成 z 轴坐标序列
+Z = list(range(0, t, 1))
+# print(Z)
+# 生成高度序列
+H = list(np.linspace(0.000000000000001,40,t))
+# print(H)
+# 生成半径序列
+R = [x/4 + (360*90)/x for x in H]
+# 限定 X 轴坐标
+x0 = np.linspace(-450,-360,n)
+x1 = np.linspace(-360,-180,360)
+temp1 = np.linspace(0,180,360)
+x2= np.linspace(-180,180,720)
+x3 = np.linspace(180,360,360)
+temp2 = np.linspace(-180,0,360)
+x4 = np.linspace(360,450,n)
+x = list(x0) + list(x1) + list(x2) + list(x3) + list(x4)
+# print(x)
+m = 0
+for i in range(len(H)):
+    z = [Z[m]]*len(x)
+    # print(z)
+    m = m + 1
+    # 一般方程
+    y0 = y4 = [H[i]]*len(x0)
+    y1 = (H[i]-R[i])+np.sqrt(np.square(R[i])-np.square(temp1))
+    y2 = R[i]-np.sqrt(np.square(R[i])-np.square(x2))
+    y3 = (H[i]-R[i])+np.sqrt(np.square(R[i])-np.square(temp2))
+    y = list(y0) + list(y1) + list(y2) + list(y3) + list(y4)
+    # print(y)
+    # 画图
+    # plt.axis('equal')
+    # plt.ylim(0, 50)
+    plt.plot(x,y)
+    # plt.plot(x0,y0)
+    # plt.plot(x1,y1)
+    # plt.plot(x2,y2)
+    # plt.plot(x3,y3)
+    # plt.plot(x4,y4)
+    plt.pause(0.05)
+plt.show() """
+
+""" """ # 创建 Excel
 parser = argparse.ArgumentParser(description = 'create excel in your select path')
 
 parser.add_argument('-i','--input', metavar='', type=str, required = True, help = 'where your want to creat excel')
@@ -35,21 +81,28 @@ if __name__ == '__main__':
         name = ["X", "Y", "Z"]
         
         print('开始计算数据并写入')
+        # 函数构建
         # 切片次数
         t = 10
+        n = 90
         # 生成 z 轴坐标序列
         Z = list(range(0, t, 1))
-        print(Z)
+        # print(Z)
         # 生成高度序列
         H = list(np.linspace(0.000000000000001,40,t))
-        print(H)
+        # print(H)
         # 生成半径序列
         R = [x/4 + (360*90)/x for x in H]
         # 限定 X 轴坐标
-        x1= np.linspace(0,180,360)
-        temp = np.linspace(-180,0,360)
-        x2 = np.linspace(180,360,360)
-        x = list(x1) + list(x2)
+        x0 = np.linspace(-450,-360,n)
+        y0 = y4 = [40.0]*len(x0)
+        x1 = np.linspace(-360,-180,360)
+        temp1 = np.linspace(0,180,360)
+        x2= np.linspace(-180,180,720)
+        x3 = np.linspace(180,360,360)
+        temp2 = np.linspace(-180,0,360)
+        x4 = np.linspace(360,450,n)
+        x = list(x0) + list(x1) + list(x2) + list(x3) + list(x4)
         # print(x)
         m = 0
         for i in range(len(H)):
@@ -57,10 +110,13 @@ if __name__ == '__main__':
             # print(z)
             m = m + 1
             # 一般方程
-            y1 = R[i]-np.sqrt(np.square(R[i])-np.square(x1))
-            y2 = (H[i]-R[i])+np.sqrt(np.square(R[i])-np.square(temp))
-            y = list(y1) + list(y2)
+            y0 = y4 = [H[i]]*len(x0)
+            y1 = (H[i]-R[i])+np.sqrt(np.square(R[i])-np.square(temp1))
+            y2 = R[i]-np.sqrt(np.square(R[i])-np.square(x2))
+            y3 = (H[i]-R[i])+np.sqrt(np.square(R[i])-np.square(temp2))
+            y = list(y0) + list(y1) + list(y2) + list(y3) + list(y4)
             # print(y)
+            # 获取表格坐标信息
             info = sht.range('A1').expand('table')
             row = info.last_cell.row
             col = info.last_cell.column
@@ -85,23 +141,13 @@ if __name__ == '__main__':
                 # 填写z坐标数据
                 sht.range((2,col+3),(2+len(x),col+3)).options(transpose = True).value = z
                 print('注入完成')
-
-        """     # 画图
-            plt.axis('equal')
-            plt.plot(x1,y1)
-            plt.plot(x2,y2)
-            plt.show()
-            plt.pause(0.05) """
     finally:
         if wb:
             wb.save(args.input)
             wb.close()
             app.kill()
-
-
-
-
-
+ """
+ """
 
 
 
